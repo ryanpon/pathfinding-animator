e8bbc624a180cf0b160c8e3d85ea6ccb8c175678
Ubuntu <ubuntu@ip-10-112-233-18.ec2.internal>
2013-03-08 11:00:31 +0000

    Make path to static assert work on both dev and prod

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 app.debug = False
 
+HOME = os.environ.get("HOME", "")
+if HOME:
+    HOME = HOME + "/"
+
 @app.route("/")
-def home():
-    with open("static/home.html") as fp:
-        return fp.read()
-
-
-@app.route("/test")
-def test():
-    with open("static/test.html") as fp:
-        return fp.read()
-
+def hello():
+    try:
+        return open(HOME + "blag/static/home.html", "r").read()
+    except Exception, e:
+        return str(e)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run()
