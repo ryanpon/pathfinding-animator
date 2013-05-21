c72875e695bbcaf76a287f3cd537079667638ced
Ryan <rpon@shortcircuit.me>
2013-05-21 16:47:40 -0700

    Support routing requests.

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
-import os, sys
-from flask import Flask, make_response
+import os 
+import sys
+try:
+    import simplejson as json
+except ImportError:
+    import json
+sys.path.append("./routing/")
+from AStar import PathfindingAnimator
+from flask import Flask, make_response, request
 app = Flask(__name__)
 app.debug = False
-
-HOME = os.environ.get("HOME", "")
-if HOME:
-    HOME = HOME + "/"
-
-if "test" in sys.argv: 
+if "test" in sys.argv:
     app.debug = True
-    HOME += "Dropbox/"
+ANIMATOR = None
+
+def split_comma_ll(ll_string):
+    s = ll_string.split(',')
+    return float(s[0]), float(s[1])
+
+def init_animator():
+    global ANIMATOR
+    with open('routing/sf.j', 'r') as fp:
+        graph = json.loads(fp.read())
+    with open('routing/sf_coords.j', 'r') as fp:
+        graph_coords = json.loads(fp.read())
+    ANIMATOR = PathfindingAnimator(graph, graph_coords)
 
 @app.route("/")
 def home():
-    return open(HOME + "blag/static/index.html", "r").read()
+    return open("static/index.html", "r").read()
 
 @app.route("/static/resume.pdf")
 def get_resume():
-    response = make_response(open(HOME + "blag/static/resume.pdf", "r").read())
+    response = make_response(open("static/resume.pdf", "r").read())
     response.headers["Content-Type"] = "application/pdf"
     return response
 
-@app.route("/static/seq.j")
-def test_seq():
-    return open(HOME + "blag/routing/seq.j", "r").read()
+@app.route("/animation")
+def search_animation():
+    search_type = request.args.get("type")
+    source = split_comma_ll(request.args.get("source"))
+    dest = split_comma_ll(request.args.get("dest"))
+    print search_type, source, dest
+    if search_type == "dijkstra":
+        seq, coords, path = ANIMATOR.dijkstra_animation(source, dest)
+    elif search_type == "astar":
+        seq, coords, path = ANIMATOR.astar_animation(source, dest)
+    elif search_type == "alt":
+        seq, coords, path = ANIMATOR.alt_animation(source, dest)
+    response = {
+        "sequence" : seq,
+        "coords" : coords,
+        "path" : path
+    }
+    return json.dumps(response)
 
 @app.route("/test")
 def test():
-    return open(HOME + "blag/static/gmaps.html", "r").read()
+    return open("static/gmaps.html", "r").read()
 
 if __name__ == "__main__":
+    init_animator()
     app.run()
