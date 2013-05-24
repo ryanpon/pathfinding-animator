600353346d8228cc1f8e1beeb61f77889788cc8e
Ryan Pon <ryan@ryan-MS-7673.(none)>
2013-05-24 14:16:07 -0700

    Add buttons to manipulate algorithm type

--- static/gmaps.js
+++ static/gmaps.js
@@ -1,30 +1,33 @@
 
 /*
-
 Goals:
--Allow visualization of graph flood
--Allow plotting of a path
-
+--buttons! options for:
+  -animation speed
+  -algorithm type
+    -different options for different algos
 */
 
 var DEFAULT_SOURCE = [37.785775,-122.40602];
 var DEFAULT_DEST = [37.773021,-122.445931];
-var s_marker;
-var e_marker;
+var sMarker;
+var eMarker;
+var shortPath;
 var predList = {};
 
-function encodeQueryData(data) {
-   var ret = [];
-   for (var d in data)
-      ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-   return ret.join("&");
+function initialize() {
+  $(".btn-group1 .btn").click(function() {
+    $(".btn-group1 .btn").removeClass("active");
+    $(this).addClass("active");
+  });
 }
 
-function initialize() {
+function initializeMap() {
+  // enable bootstrap buttons
   var mapOptions = {
     center: new google.maps.LatLng(37.774,-122.420),
     zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true
   };
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   setMarkers(DEFAULT_SOURCE, DEFAULT_DEST);
@@ -35,14 +38,23 @@ function createLL(p) {
   return new google.maps.LatLng(p[0], p[1]);
 }
 
+function encodeQueryData(data) {
+   var ret = [];
+   for (var d in data)
+      ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+   return ret.join("&");
+}
+
 function requestSequence(map) {
   // load the sequence
-  //var url = "http://localhost:5000/animation";
-  var url = "http://ryanpon.com/animation";
-  var source = s_marker.getPosition();
-  var dest = e_marker.getPosition();
+  var url = "http://localhost:5000/animation";
+  //var url = "http://ryanpon.com/animation";
+  var source = sMarker.getPosition();
+  var dest = eMarker.getPosition();
+  var algo = $(".btn-group .active").data("value");
+  console.log(algo);
   var data = {
-    "type": "astar",
+    "type": algo,
     "source": source.lat() + ',' + source.lng(),
     "dest": dest.lat() + ',' + dest.lng()
   };
@@ -54,13 +66,15 @@ function doAnimation(data, map) {
   resetMapLines();
   var sequence = data["sequence"];
   var nodeCoords = data["coords"];
+  var path = data["path"];
+  drawPath(path, map);
   var wait = 0;
   for (var i = 0; i < sequence.length; ++i) {
     var node = sequence[i][0], actions = sequence[i][1];
     for (var k = 0; k < actions.length; ++k) {
       var edge = actions[k];
       drawAnimation(node, edge, nodeCoords, wait);
-      wait += 5;
+      wait += 0;
     }
   }
 }
@@ -72,20 +86,29 @@ function drawAnimation(node, edge, nodeCoords, wait) {
       predList[edge] = null;
     }
     var path = [createLL(nodeCoords[node]), createLL(nodeCoords[edge])];
-    var pLine = createPolyline(path, "#FF0000");
+    var pLine = createPolyline(path, "#FF0000", 1);
     predList[edge] = pLine;
     pLine.setMap(map);
   }, wait);
 }
 
+function drawPath(path, map) {
+  var googleLLPath = [];
+  for (var i = 0; i < path.length; ++i) {
+    googleLLPath.push(createLL(path[i]));
+  }
+  shortPath = createPolyline(googleLLPath, "#000000", 2);
+  shortPath.setMap(map);
+}
+
 function setMarkers(source, dest) {
-  s_marker = new google.maps.Marker({
+  sMarker = new google.maps.Marker({
     position: createLL(source),
     map: map,
     title:"Start",
     draggable: true
   });
-  e_marker = new google.maps.Marker({
+  eMarker = new google.maps.Marker({
     position: createLL(dest),
     map: map,
     title:"End",
@@ -94,10 +117,10 @@ function setMarkers(source, dest) {
 }
 
 function setMarkerEvents(map) {
-  google.maps.event.addListener(s_marker, 'dragend', function() {
+  google.maps.event.addListener(sMarker, 'dragend', function() {
     requestSequence(map);
   });
-  google.maps.event.addListener(e_marker, 'dragend', function() {
+  google.maps.event.addListener(eMarker, 'dragend', function() {
     requestSequence(map);
   });
 }
@@ -107,14 +130,18 @@ function resetMapLines() {
     predList[line].setMap(null);
   }
   predList = {};
+  if (shortPath) {
+    shortPath.setMap(null);
+  }
+
 }
 
-function createPolyline(path, color) {
+function createPolyline(path, color, size) {
   pLine = new google.maps.Polyline({
     path: path,
     strokeColor: color,
     strokeOpacity: .7,
-    strokeWeight: 1 
+    strokeWeight: size 
   });
   return pLine
 }
@@ -128,4 +155,22 @@ function setPolylineListener(pLine, map) {
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+/*function testMarkers(map) {
+  var markers = [];
+  for (var i = 0; i < POINTS.length; ++i) {
+    var m = new google.maps.Marker({
+      position: createLL(POINTS[i]),
+      map: map,
+      title:"Start",
+      draggable: true
+    });
+    markers.push(m);
+  };
+  return markers;
+}*/
+
+google.maps.event.addDomListener(window, 'load', initializeMap);
+
+$(document).ready(function(){
+  initialize();
+});
