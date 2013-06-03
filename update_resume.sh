da86859590b1591bed873dafe00abcddb3d1de28
Ryan Pon <rpon00@gmail.com>
2013-06-03 16:58:44 -0700

    Remove old files

--- update_resume.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -H "Content-Type: application/pdf" https://writelatex.s3.amazonaws.com/79748zgkjgv/output/fe64560329903c3c357c098c934ce4c88d437f37/79748zgkjgv.pdf -o ~/blag/static/resume.pdf
